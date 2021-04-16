9e4ac81d3c1b7c817d8bf0dd08ff65ef029eba58
Sai Ashish <51746934+SaiAshish-Konchada@users.noreply.github.com>
2021-04-16 16:10:58 +0530

    Create Zip & Unzip.py

---
+++ 5 More Python Projects in 5 Minutes/Zip & Unzip.py
@@ -0,0 +1,14 @@
+import patoolib
+
+def Zip():
+    patoolib.create_archive("file.zip",("path_to_file_1","path_to_file_2"))
+
+def Unzip():
+    patoolib.extract_archive("path_to_file_to_be_extracted",outdir="extract")
+
+if __name__ == '__main__':
+    choice=(int(input(Enter 1 for Zip & 2 for Unzip)))
+    if choice==1:
+        print(Zip())
+    else:
+        print(Unzip())
